e8f5ead998f18a982631e61ed775078751df1814
Chris Olmos <christianolmos@protonmail.com>
2020-04-26 17:40:40 -0500

    A 3 page simple react-bootstrap template, which took one hour

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import {Jumbotron as Jumbo, Container} from 'react-bootstrap';
 import styled from 'styled-components';
-import boatImage from '../assets/boatImage.jpg'
+import jumbobackground from '../assets/jumbobackground.jpg'
 
                                     // again, this styled.div is just a massive string inside backticks.
 const Styles = styled.div `
     .jumbo {
 
-        background:url(${boatImage}) no-repeat fixed bottom;
+        background:url(${jumbobackground}) no-repeat fixed bottom;
         background-size: cover;
         color: #efefef;
         height: 200px;
