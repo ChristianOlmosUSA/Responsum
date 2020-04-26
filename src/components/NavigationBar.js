29fc7b8eabece1f7bffe130f42f26166a228302c
Chris Olmos <christianolmos@protonmail.com>
2020-04-26 17:50:57 -0500

    Simple Template, navbar text hover color is crimson red

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import {Nav, Navbar} from 'react-bootstrap';
 import styled from 'styled-components';
+import NavStyles from './NavStyles.css';
 
                                                 //  backtick makes this all one giant string basically 
-const Styles = styled.div`      
-    .navbar{
-        background-color: #222;
-        
+const Styles = styled.div`
+  .navbar {
+    background-color: #222;
+  }
+
+  a, .navbar-brand, .navbar-nav .nav-link {
+    color: #bbb;
+    
+    &:hover {
+      color: crimson;
+     }
     }
-
-    .navbar-brand, .navbar-nav .navlink {
-        color : #bbb;
-
-        &: hover {
-            color: white;
-        }
-    }
-
-`;
+  `;
 
 export const NavigationBar = () => (
     <Styles>
